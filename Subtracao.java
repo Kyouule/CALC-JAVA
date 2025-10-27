5fae0dbc959998014fd10948fc80e74ec76cdd7f
Marcus Vinicius <marcuss213@outlook.com>
2025-10-27 19:48:21 -0300

    Update Subtracao.java

--- Subtracao.java
+++ Subtracao.java
@@ -1,4 +1,4 @@
-public class Subtracao implements OperacaoMatematica {
+public class Subtracao implements Operacao {
     @Override
     public double executar(double num1, double num2) {
         return num1 - num2;
