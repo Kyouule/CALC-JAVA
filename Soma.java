030b0cacd3a074a3d9f43500cbe23ae00b85ddf8
Marcus Vinicius <marcuss213@outlook.com>
2025-10-27 19:47:34 -0300

    Update Soma.java

--- Soma.java
+++ Soma.java
@@ -1,4 +1,4 @@
-public class Soma implements OperacaoMatematica {
+public class Soma implements operacao {
     @Override
     public double executar(double num1, double num2) {
         return num1 + num2;
