9c5591e980784709aaca717afdfc8185201c082f
Marcus Vinicius <marcuss213@outlook.com>
2025-10-27 19:59:51 -0300

    Update calculadora.java

--- calculadora.java
+++ calculadora.java
@@ -1,6 +1,6 @@
 public class Calculadora {
 
-    public double calcular(double num1, double num2, OperacaoMatematica operacao) {
+    public double calcular(double num1, double num2, Operacao operacao) {
         return operacao.executar(num1, num2);
     }
 }
