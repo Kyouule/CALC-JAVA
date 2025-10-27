94dc5dcb231c348734fa6563c722b48534511509
Marcus Vinicius <marcuss213@outlook.com>
2025-10-27 19:45:28 -0300

    Create Multiplicacao.java

---
+++ Multiplicacao.java
@@ -0,0 +1,11 @@
+public class Multiplicacao implements OperacaoMatematica {
+    @Override
+    public double executar(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public String getSimbolo() {
+        return "*";
+    }
+}
